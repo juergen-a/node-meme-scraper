519a245709fb06c5dec3d6b2c601a5bd3bc6827c
Juergen <juergen.a2025@gmail.com>
2025-01-22 02:38:14 +0100

    Updated program code, downloaded images, deleted unused libraries

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Node.js meme-scraper
+
+## TODOs
+
+- [ ] Figure out how to make `memes` "ignored" in Git
+- [ ] Create a `memes` directory
+
+- [ ] Investigate how to access the website
+
+  - [ ] Is "HTTP request" or "fetching" what we want?
+
+- [ ] Retrieve the HTML string data from the website and store in a variable
+
+- [ ] Extract the image `src` strings (URLs) in the HTML string (`<img>` elements) to an array of image URLs
+
+  - [ ] Check whether the image `src` URLs work with the `?width=300`
+
+- [ ] Separate out the first 10 image URLs from the array
+- [ ] Loop over first 10 image URLs, and for each of them:
+
+  - [ ] Access the image URL
+
+    - [ ] Is "HTTP request" or "fetching" what we want?
+
+  - [ ] Retrieve the image data from the website and store in a variable
+
+  - [ ] Save the the image data to a file
+    - [ ] Create a new file with a name with a 2-digit number, eg. 01.jpg, 02.jpg, ... 10.jpg.
+    - [ ] Add the data
+
+- [ ] Test program to make sure it can run multiple times
+
+Stretch goals:
+
+- [ ] Make the application create your own custom meme (eg. `node index.js hello karl bender` would download an image with the top text of "hello", the bottom text of "karl", with the meme image of Bender)
+- [ ] Add a nice progress indicator (either messages or a progress bar)
+- [ ] Create a version of your program that uses [Deno](https://deno.com/) or [Bun](https://bun.sh/) instead of Node.js
+- [ ] Make your program run on any computer without Node.js or any other runtime installed by creating a [Node.js Single Executable Application](https://nodejs.org/api/single-executable-applications.html), [Deno standalone executable](https://docs.deno.com/runtime/reference/cli/compiler/) or [Bun single-file executable](https://bun.sh/docs/bundler/executables)
+
+## Acceptance Criteria
+
+- [ ] Preflight runs through without errors in your project
+  - [ ] Link in your GitHub repo's About section: Replit demo
+- [ ] [Drone bot](https://learn.upleveled.io/pern-extensive-immersive/modules/cheatsheet-tasks/#upleveled-drone) has been tagged and responded with a passing message
+- [ ] Correct GitHub commit message format (see [Writing Commit Messages](https://learn.upleveled.io/pern-extensive-immersive/modules/cheatsheet-git-github/#writing-commit-messages))
---
+++ index.js
@@ -0,0 +1,78 @@
+// Import libraries as node js has no access to client side DOM methods
+
+// Module to write data to files and store in folders
+import fs from 'node:fs';
+// A virtual interface to retrieve data from the DOM
+import { JSDOM } from 'jsdom';
+// Http-client for fetch requests - enables to retrieve binary data formats through the arrayBuffer() method
+import fetch from 'node-fetch';
+
+// Website to retrieve images from
+const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+// Fetch website data from the DOM in string-format via response object.data
+function run() {
+  fetch(url)
+    .then(function (futureData) {
+      return futureData.text();
+    })
+
+    .then(function (text) {
+      // Virtual access to the DOM of the fetched website data
+      const dom = new JSDOM(text);
+      const document = dom.window.document;
+
+      // Extract <img> tags and create an array of <img> elements
+      const imgTags = document.getElementsByTagName('img');
+      const imgTagsArr = Array.from(imgTags);
+
+      // Extract values from the 'src' attribute of gathered <img> tags and return an array of strings
+      const imgUrlArr = [];
+      imgTagsArr.forEach(function (img) {
+        if (img.getAttribute('src').endsWith('.jpg?width=300')) {
+          imgUrlArr.push(img.getAttribute('src'));
+        }
+      });
+
+      // Reduce # of links to pictures to 10
+      const imageUrls = imgUrlArr.slice(0, 10);
+
+      // Evoking to fetch, download and store image data to the directory
+      function downloadImg(img, fileName) {
+        // Fetching image data
+        fetch(img)
+          .then(function (response) {
+            return response.arrayBuffer();
+          })
+
+          // Accesing the binary img-data of the response object with the arrayBuffer() method to store the data with Buffer
+          .then(function (arrayBuffer) {
+            const buffer = Buffer.from(arrayBuffer);
+            fs.writeFile(
+              `/Users/juergen/projects/node-meme-scraper/memes/${fileName}`,
+              buffer,
+              (err) => {
+                if (err) {
+                  console.error(err);
+                } else {
+                  // Response object.value = resolved
+                }
+              },
+            );
+          })
+          .catch(function (error) {
+            console.log('Error downloading file:', error);
+          });
+      }
+      // Calling 'downloadImg' function within loop to get every img 1)named and 2)saved by 'downloadImg' function
+      imageUrls.forEach(function (img, i) {
+        const fileName = String(i + 1).padStart(2, '0') + '.jpg';
+        downloadImg(img, fileName);
+      });
+    })
+
+    .catch(function (error) {
+      console.log('Error ocurred:', error);
+    });
+}
+run();
